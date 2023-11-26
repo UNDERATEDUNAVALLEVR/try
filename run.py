ff8bdcff8f7fd1ce8eda7ab4d0d571df1af7a9aa
UNDERATEDUNAVALLEVR <124512923+UNDERATEDUNAVALLEVR@users.noreply.github.com>
2023-11-26 12:57:14 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,32 @@
+data = {
+                'lsd': lsd2,
+                'jazoest': j2,
+                'ccp': ccp,
+                'reg_instance': reg_i,
+                'submission_request': 'true',
+                'helper': '',
+                'reg_impression_id': reg_id,
+                'ns': '0',
+                'zero_header_af_client': '',
+                'app_id': '',
+                'logger_id': '',
+                'field_names[]': [
+                    'firstname',
+                    'reg_email__',
+                    'sex',
+                    'birthday_wrapper',
+                    'reg_passwd__'
+                ],
+                'firstname': nmf,
+                'lastname': nml,
+                'reg_email__': ids,
+                'sex': str(random.choice(['1','2'])),
+                'custom_gender': '',
+                'did_use_age': 'false',
+                'birthday_day': dayy,
+                'birthday_month': monn,
+                'birthday_year': yearr,
+                'age_step_input': '',
+                'reg_passwd__': pswrd,
+                'submit': 'Sign Up'
+}
